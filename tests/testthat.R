6a4c3f652bb6d57880fcd84b63301eb1ae79960b
Ben Raymond <ben.raymond@aad.gov.au>
2016-10-12 09:23:54 +1100

    tests updated

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,4 +2,5 @@ library(testthat)
 library(dplyr)
 library(cga)
 
+g <- load_cga()
 test_check("cga")
--- tests/testthat/test-cga.R
+++ tests/testthat/test-cga.R
@@ -1,7 +1,6 @@
 context("CGA")
 
 test_that("subsetting works", {
-    g <- load_cga()
     expect_equal(nrow(names_near(g,c(100,-66),20)),5)
     expect_gt(nrow(g %>% names_near(c(100,-66),20)),nrow(g %>% names_near(c(100,-66),20) %>% subset_names(feature_type="Island")))
 })
