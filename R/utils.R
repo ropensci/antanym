15e660c35d79a52d189452920720b2281c840dee
Ben Raymond <ben.raymond@aad.gov.au>
2016-10-12 12:44:35 +1100

    helper to find preferred names

---
+++ R/utils.R
@@ -0,0 +1,23 @@
+#' Find one name per feature according to specified preferences
+#'
+#' @references \url{http://www.scar.org/data-products/cga}
+#' @param gaz data.frame: as returned by \code{\link{agn_read}}
+#' @param origin_country character: vector of country names. If a given feature has been named by one of these countries, this name will be returned. For valid values see \code{\link{agn_countries}}
+#'
+#' @return data.frame of results
+#'
+#' @seealso \code{\link{agn_read}}
+#'
+#' @examples
+#' \dontrun{
+#'  g <- agn_read(cache_directory="c:/temp/gaz")
+#'  g %>% agn_filter("^Ufs") %>% agn_preferred(origin_country="United States of America")
+#' }
+#'
+#' @export
+agn_preferred <- function(gaz,origin_country) {
+   pn1 <- gaz %>% group_by_("scar_common_id") %>% filter_(~country_name %in% origin_country) %>% slice(1L)
+   pn2 <- gaz %>% group_by_("scar_common_id") %>% filter_(~!country_name %in% origin_country) %>% slice(1L)
+   bind_rows(pn1,pn2 %>% filter_(~!scar_common_id %in% pn1$scar_common_id))
+}
+
