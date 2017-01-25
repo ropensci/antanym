4debf9d3aa340b9baf8808c2097a5761a388a83c
Ben Raymond <ben.raymond@aad.gov.au>
2017-01-25 11:13:57 +1100

    fix up initial bugs in sp tests

--- tests/testthat/test-sp.R
+++ tests/testthat/test-sp.R
@@ -1,22 +1,24 @@
-context("antanym")
+context("antanym spatial objects")
+
+gsp <- an_read(sp=TRUE)
+testpt <- c(110,-66)
+namelist <- function(z)sort(z$place_name)
 
 test_that("sp option returns an sp object", {
-    gsp <- an_read(sp=TRUE)
     expect_s4_class(gsp,"SpatialPointsDataFrame")
 })
 
 test_that("sp versions of functions match non-sp for lon-lat data", {
     ext <- c(100,110,-70,-60)
     expect_gt(nrow(an_filter(g,extent=ext)),100)
-    expect_equal(nrow(an_filter(g,extent=ext)),nrow(an_filter(gsp,extent=ext)))
+    expect_identical(namelist(an_filter(g,extent=ext)),namelist(an_filter(gsp,extent=ext)))
     expect_identical(an_countries(g),an_countries(gsp))
     expect_identical(an_feature_types(g),an_feature_types(gsp))
     expect_identical(an_cga_sources(g),an_cga_sources(gsp))
 
-    pt <- c(110,-66)
-    namelist <- function(z)sort(z$place_name)
+    pt <- testpt
     expect_identical(namelist(an_near(g,pt,50)),namelist(an_near(gsp,pt,50)))
-    pt <- SpatialPoints(cbind(110,-66))
+    pt <- SpatialPoints(cbind(testpt[1],testpt[2]))
     projection(pt) <- "+proj=longlat +datum=WGS84 +ellps=WGS84"
     expect_identical(namelist(an_near(g,pt,50)),namelist(an_near(gsp,pt,50)))
 })
@@ -28,7 +30,8 @@ test_that("sp versions of functions match non-sp for projected data", {
     expect_identical(an_feature_types(g),an_feature_types(gsp2))
     expect_identical(an_cga_sources(g),an_cga_sources(gsp2))
 
-    pt2 <- SpatialPoints(cbind(110,-66))
+    pt <- testpt
+    pt2 <- SpatialPoints(cbind(testpt[1],testpt[2]))
     projection(pt2) <- "+proj=longlat +datum=WGS84 +ellps=WGS84"
     pt2 <- spTransform(pt2,stersouth)
     expect_identical(namelist(an_near(g,pt,20)),namelist(an_near(gsp2,pt2,20)))
