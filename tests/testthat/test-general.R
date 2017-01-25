87697841797e075e5512297c008fac1d0bfaed05
Ben Raymond <ben.raymond@aad.gov.au>
2017-01-25 11:41:47 +1100

    add test for gaz_id

---
+++ tests/testthat/test-general.R
@@ -0,0 +1,8 @@
+context("antanym data structures")
+
+test_that("gaz_id is present and unique", {
+    ## some functions rely on gaz_id, check that it's present and unique
+    expect_true("gaz_id" %in% names(g))
+    expect_false(any(duplicated(g$gaz_id)))
+})
+
