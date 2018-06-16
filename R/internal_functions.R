df88464154cce9e48cefee51d64edfbb2556f3bc
Ben Raymond <ben.raymond@aad.gov.au>
2018-06-16 15:26:44 +1000

    stupid typo

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -1,4 +1,4 @@
 ## internal functions, not exported to the user
 
 ## wrapper on na.omit that silences warnings when applied to NULL objects
-no_na <- function(z) suppressWarnings(na.omit(x))
+no_na <- function(z) suppressWarnings(na.omit(z))
