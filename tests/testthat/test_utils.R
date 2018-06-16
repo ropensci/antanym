11bd6ce428d2394b85985ca55159464e351e404d
Ben Raymond <ben.raymond@aad.gov.au>
2018-06-16 14:05:42 +1000

    typo

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,6 +1,6 @@
 context("tests of general antanym utility functions")
 
-test_that("list of gazetters is as expected",{
+test_that("list of gazetteers is as expected",{
     expect_identical(an_gazetteers(), c("CGA"))
 })
 
